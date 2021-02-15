040ca96536a4530f4ddfa0a35db1ddd0e37da110
Gabriel Santos <gabrielbarretos2000@outlook.com>
2021-02-15 19:08:47 -0300

    feat(api): api created

--- src/main/java/com/nextplugins/nextwarps/api/NextWarpAPI.java
+++ src/main/java/com/nextplugins/nextwarps/api/NextWarpAPI.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class NextWarpAPI {
@@ -16,16 +18,65 @@ public final class NextWarpAPI {
 
     @Getter private static final NextWarpAPI instance = new NextWarpAPI();
 
+    /**
+     * Search all warps to look for one with the entered name.
+     *
+     * @param warp warp name
+     * @return {@link Optional} with the warp
+     */
     public Optional<Warp> findWarpByName(String warp) {
-        Set<Warp> warps = plugin.getWarpAdapter().getWarps();
-
         Warp target = null;
 
-        for (Warp pluginWarp : warps) {
+        for (Warp pluginWarp : allWarps()) {
             if (pluginWarp.getName().equalsIgnoreCase(warp)) target = pluginWarp;
         }
 
         return Optional.ofNullable(target);
+    }
+
+    /**
+     * Search all warps to look for one with the entered custom filter.
+     *
+     * @param filter custom filter
+     * @return {@link Optional} with the warp
+     */
+    public Optional<Warp> findWarpByFilter(Predicate<Warp> filter) {
+        return allWarps().stream()
+                .filter(filter)
+                .findFirst();
+    }
+
+    /**
+     * Retrieve all warps with the custom permission.
+     *
+     * @param permission custom permission to search
+     * @return {@link Set} with warps
+     */
+    public Set<Warp> findWarpsByPermission(String permission) {
+        return allWarps().stream()
+                .filter(warp -> warp.getPermission().equalsIgnoreCase(permission))
+                .collect(Collectors.toSet());
+    }
+
+    /**
+     * Search all warps to look for every with the entered custom filter.
+     *
+     * @param filter custom filter to search
+     * @return {@link Set} with warps
+     */
+    public Set<Warp> findWarpsByFilter(Predicate<Warp> filter) {
+        return allWarps().stream()
+                .filter(filter)
+                .collect(Collectors.toSet());
+    }
+
+    /**
+     * Retrieve all warps loaded so far.
+     *
+     * @return {@link Set} with warps
+     */
+    public Set<Warp> allWarps() {
+        return plugin.getWarpAdapter().getWarps();
     }
 
 }
